8d508bde9c81e556767f404c20c414455237dbb2
Kavin <87133047+kavin-1408@users.noreply.github.com>
2024-05-15 15:28:54 +0530

    Add files via upload

---
+++ ToolWindow/ToolWindow/ToolTimer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ToolWindow
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("8a854690-b9a9-43ee-ba20-c956fb24f1ae")]
+    public class ToolTimer : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolTimer"/> class.
+        /// </summary>
+        public ToolTimer() : base(null)
+        {
+            this.Caption = "ToolTimer";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ToolTimerControl();
+        }
+    }
+}
---
+++ ToolWindow/ToolWindow/ToolTimerControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToolWindow
+{
+    /// <summary>
+    /// Interaction logic for ToolTimerControl.
+    /// </summary>
+    public partial class ToolTimerControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolTimerControl"/> class.
+        /// </summary>
+        public ToolTimerControl()
+        {
+            InitializeComponent();
+           //webView.Source = new Uri("https://example.com");
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "ToolTimer");
+        }
+    }
+}
